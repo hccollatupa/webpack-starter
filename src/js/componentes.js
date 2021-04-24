21774c7932ee82ee07bd7055adba33efee351d67
Hector Ccollatupa <hccollatupa@gmail.com>
2021-04-24 02:01:51 -0500

    Webpack Dev Server

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,5 +1,5 @@
 export const saludar = (nombre) => {
-    console.log('Creando etiqueta h1');
+    console.log('Creando etiqueta h1 en el HTML');
 
     const h1 = document.createElement('h1');
     h1.innerText = `Hola, ${nombre}`;
